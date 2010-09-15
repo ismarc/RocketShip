2f5e8c2a6bde6d52f481b80bf05b1d45b7a62d0d
Matthew Brace <ismarc31@gmail.com>
2010-09-15 02:50:26 -0500

    Remove function definitions for removed functions

--- RocketShip.h
+++ RocketShip.h
@@ -57,16 +57,6 @@ namespace rocketship {
         void processFunction(Function &F);
         void processBlock(BasicBlock* bblock, pBlock block);
         void processInstruction(Instruction* instruction, pNode node);
-        void processFunctionOld(Function &F);
-        /**
-         * Generates the node structure of the given function with the supplied node
-         * as the base node.  Each block in the function is processed in the order it
-         * appears in the bytecode and linked together by branch and unconditional
-         * branch instructions.
-         * @param funcNode The node object representing the base node to use.
-         * @param F The function to generate the node structure for.
-         */
-        void generateNodeStructure(Node* funcNode, Function &F);
 
         /**
          * Outputs a single node to the output filestream base on the node type and
@@ -75,108 +65,8 @@ namespace rocketship {
          */
         void emitNode(Node* node);
 
-        void processFunctionBlocks(Node* funcNode, BasicBlock* block);
-        /**
-         * Processes a single block for a function.  Creates a node for each appropriate
-         * instruction in the block and edges connecting them.  The node is appended to
-         * the module _nodes list for emitting later.
-         * @param funcNode The base node of the function.
-         * @param currentNode The current node when the block begins processing.
-         * @param block The code block to process.
-         */
-        void processFunctionBlock(Node* funcNode, Node* currentNode, BasicBlock* block);
-
-        /**
-         * Handles a call instruction encountered in the function block.  Call
-         * instructions have special display formatting to more easily present the
-         * actual call that is being made.
-         * @param instructionNode The node representing the call instruction.
-         * @param currentNode The current node prior to the call instruction.
-         * @param callInst The internal representation of the call instruction.
-         * @return The next node to use in the graph.
-         */
-        Node* processCallInstruction(Node* instructionNode,
-                                     Node* currentNode,
-                                     CallInst* callInst);
-
         std::string getConditionalBranchLabel(BranchInst* instruction);
         std::string getInvokeInstLabel(InvokeInst* instruction);
-        /**
-         * Handles a conditional branch instruction encountered in the function block.
-         * Conditional branch instructions represent a branch based on a comparison that
-         * resolves to a true or false result.  Both possible subsequent nodes need to
-         * be added to the graph.
-         * @param instructionNode The node representating the conditional branch.
-         * @param currentNode The current node prior to the branch instruction.
-         * @param branchInst The internal representation of the branch instruction.
-         * @param functionName The name of the function currently being processed
-         *                     (used as part of uniquely identifying blocks).
-         * @return The next node to use in the graph.
-         */
-        Node* processConditionalBranch(Node* instructionNode,
-                                       Node* currentNode,
-                                       BranchInst* branchInst,
-                                       std::string functionName);
-
-        /**
-         * Handles an unconditional branch instruction (similar to a jump) in the
-         * function block.  Unconditional branches are jumps to the label specified.
-         * Displaying them within the graph is extraneous information, so the label is
-         * pushed into the graph rather than the branch node.
-         * @param instructionNode The node representating the unconditional branch.
-         * @param currentNode The current node prior to the branch instruction.
-         * @param branchInst The internal representation of the branch instruction.
-         * @param functionName The name of the function currently being processed
-         *                     (used as part of uniquely identifying blocks).
-         * @return The next node to use in the graph.
-         */
-        Node* processUnconditionalBranch(Node* instructionNode,
-                                         Node* currentNode,
-                                         BranchInst* branchInst,
-                                         std::string functionName);
-
-        /**
-         * Handles a store instruction in the function block.  Store instructions have
-         * a special label display to remove ambiguities of what value is stored in which
-         * location.
-         * @param instructionNode The node representing the store instruction.
-         * @param currentNode The current node prior to the store instruction.
-         * @param storeInst The internal representation of the store instruction.
-         * @return The next node to use in the graph.
-         */
-        Node* processStoreInstruction(Node* instructionNode,
-                                      Node* currentNode,
-                                      StoreInst* storeInst);
-
-        /**
-         * Handles switch instructions in the function block.  A
-         * switch instruction indicates a multi-path branch based on
-         * the input value.  The decision node with indicate the value
-         * that is checked and the edge will indicate the branch that
-         * is followed if the value matches.
-         * @param instructionNode The node representing the switch instruction.
-         * @param currentNode The current node prior to the switch instruction.
-         * @param switchInst The internal representation of the switch instruction
-         * @param functionName The name of the function currently being processed
-         *                     (used as part of uniquely identifying blocks).
-         * @return The next node to use in the graph.
-         */
-        Node* processSwitchInstruction(Node* instructionNode,
-                                       Node* currentNode,
-                                       SwitchInst* switchInst,
-                                       std::string functionName);
-        /**
-         * Handles all instructions that do not have a special case.  A general
-         * instruction is considered an activity and has a label that is defined as:
-         * opcode operand(0) operand(1) operand(n).
-         * @param instructionNode The node representing the instruction.
-         * @param currentNode The current node prior to the instruction.
-         * @param instruction The internal representation of the instruction.
-         * @return The next node to use in the graph.
-         */
-        Node* processDefaultInstruction(Node* instructionNode,
-                                        Node* currentNode,
-                                        Instruction* instruction);
 
         std::string getDemangledName(std::string name);
         std::string getLabelForNode(Instruction* instruction);
@@ -184,22 +74,9 @@ namespace rocketship {
         std::string getCallInstructionLabel(CallInst* instruction);
         std::string getSwitchInstLabel(SwitchInst* instruction);
         std::string getStoreInstLabel(StoreInst* instruction);
-        /**
-         * Stores the list of nodes to draw in the graph.  DOT file formats use a
-         * labelling mechanism so the ordering of entries in _nodes doesn't matter.  This
-         * simplifies the process of building the graph since the actual graph structure
-         * doesn't need represented.  Only a list of nodes and the edges the nodes have is
-         * needed.
-         */
-        std::vector<Node*> _nodes;
-        std::vector<pNode> _pnodes;
-        /**
-         * Stores the list of blocks that have been processed for the
-         * current function.
-         */
-        std::map<BasicBlock*, int> _processedBlocks;
 
-        std::map<int, BasicBlock*> _pendingEdges;
+        std::vector<pNode> _pnodes;
+
         /**
          * Stores the next id to use for a node.  Since few nodes will have unique names,
          * and DOT files require each node to have a unique name, the name of the node is
